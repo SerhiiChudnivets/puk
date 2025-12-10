f991f991b804df63db7fa2f53649de7f75319c1c
SerhiiChudnivers <serhiichudnivets@gmail.com>
2025-12-10 17:34:18 +0200

    чччччччdddddd

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/puk/',   // <-- ОБОВʼЯЗКОВО
-
   server: {
     host: "::",
     port: 8080,
